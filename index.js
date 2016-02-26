bea26146856c30da92204399cea53664e5d9f39f
Monty Anderson <monty.linux@gmail.com>
2016-02-26 21:43:14 +0000

    it works!

---
+++ index.js
@@ -0,0 +1,41 @@
+'use strict'
+const path = require("path");
+const request = require("request");
+
+const endpoints = {
+    block: "block",
+    blockIndex: "block-index",
+    tx: "tx",
+    rawtx: "rawtx",
+    addr: "addr",
+    addrs: "addrs",
+    txs: "txs",
+    sync: "sync",
+    peer: "peer"
+}
+
+const insight = module.exports = function(url) {
+    this.url = url;
+};
+
+for(let key in endpoints) {
+    insight.prototype[key] = function() {
+        const args = Array.apply(null, arguments);
+        let callback = null;
+
+        if(typeof args[args.length - 1] == "function") {
+            callback = args.splice(args.length - 1, 1)[0];
+        }
+
+        let url = this.url + "/" + endpoints[key];
+
+        for(let i in args) {
+            if(typeof i !== "function") url += "/" + args[i];
+        }
+
+        request(url, function(err, res, body) {
+            if(err) return callback(err);
+            if(callback) callback(null, JSON.parse(body));
+        });
+    }
+}
--- test.js
+++ test.js
@@ -1,13 +1,3 @@
-# insight-client
-A Node.JS client for the Bitcore Insight API.
-
-## Getting Started
-
-```
-$ npm install insight-client --save
-```
-
-``` javascript
 const insight = require("./");
 
 const node = new insight("https://insight.bitpay.com/api");
@@ -15,5 +5,3 @@ const node = new insight("https://insight.bitpay.com/api");
 node.blockIndex(100, function(err, data) {
     console.log(err || data);
 });
-
-```
