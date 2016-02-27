8b0331fee7a05b3c43d0d5a23054330a9e2966ca
Monty Anderson <monty.linux@gmail.com>
2016-02-27 13:21:17 +0000

    travis ci + test script

--- test.js
+++ test.js
@@ -1,7 +1,8 @@
+const assert = require("assert");
 const insight = require("./");
 
 const node = new insight("https://insight.bitpay.com/api");
 
 node.blockIndex(100, function(err, data) {
-    console.log(err || data);
+    assert(data.blockHash, "000000007bc154e0fa7ea32218a72fe2c1bb9f86cf8c9ebf9a715ed27fdb229a");
 });
